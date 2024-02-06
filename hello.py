00d3c42761afb6f70a95ddf8f967e28c59cd6309
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-02-06 12:07:48 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
-#variables
-length = 10
-width = 3
-area = 30
-print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area))
+#define variables ofr Kilogram (kg) values
+kg_value_1 = 50
+kg_value_2 = 926
+kg_value_3 = 43.9
+
+#conversion factor: 1 pound (lb) = Kilograms (kg) * 2.20462
+conversion_factor = 2.20462
+
+#calculate pounds for each kilogram value
+pounds_1 = kg_value_1 * conversion_factor
+pounds_2 = kg_value_2 * conversion_factor
+pounds_3 = kg_value_3 * conversion_factor
+
+#output the results
+print(f"{kg_value_1} kilograms is equal to {pounds_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_3:.2f} puounds.")
