f0ab28a0fc454aeeec87d2d8dc674a23cbcc9ba8
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-02-12 11:33:09 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
-#Enter the numeric grade you recieved
-numeric_grade = (79)
-print("Enter the numeric grade:")
-print("" + str(numeric_grade) + "")
-if numeric_grade > 89:
-    print("The letter grade is A.")
-elif numeric_grade > 79:
-    print("The letter grade is B.")
-elif numeric_grade > 69:
-    print("The letter grade is C.")
-elif numeric_grade > 59:
-    print("The letter grade is D.")
-elif numeric_grade < 51:
-    print("The letter grade is F.")
+# starting count of beer on the wall
+count = 99
+while count > 2:
+    print(count, "bottles of beer on the wall")
+    print( count, "bottles of beer")
+    print("take one down, pass it around")
+    count -= 1
+    print(count, "bottles of beer on the wall!")
+print("2 bottles of beer on the wall")
+print("2 bottles of beer")
+print("take one down, pass it around")
+print("1 bottle of beer on the wall!")
+print("1 bottle of beer on the wall")
+print("1 bottle of beer")
+print("take one down, pass it around")
+print("No bottles of beer on the wall!")
