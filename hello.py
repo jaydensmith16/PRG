2d8a966ffff6b687e7219e7e9dc95a41079e5ad9
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-02-19 10:51:34 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,16 +1,18 @@
-# starting count of beer on the wall
-count = 99
-while count > 2:
-    print(count, "bottles of beer on the wall")
-    print( count, "bottles of beer")
-    print("take one down, pass it around")
-    count -= 1
-    print(count, "bottles of beer on the wall!")
-print("2 bottles of beer on the wall")
-print("2 bottles of beer")
-print("take one down, pass it around")
-print("1 bottle of beer on the wall!")
-print("1 bottle of beer on the wall")
-print("1 bottle of beer")
-print("take one down, pass it around")
-print("No bottles of beer on the wall!")
+# Days of the week
+days = ["Monday", "Tuesday"," Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# how many steps the person took per day
+step_num = [1200, 3400, 2000, 900, 910, 1000, 800]
+# The numbers are in place of the numbers someone would be putting into this code
+
+for i in range(len(days)):
+    day = days[i]
+    steps = step_num[i]
+    # takes number of steps from the steps list
+    print(f"On {day} how many steps did you take?")
+    print(f"{steps}")
+# total step calculation
+total_steps = sum(step_num)
+print(f"The total steps for the week is {total_steps}.")
+# average steps per day
+average = (total_steps // 7)
+print(f"The average steps per day is {average}.")
