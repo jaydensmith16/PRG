b082ab66d14f9dde0817ae2b6330627e42d5467f
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-02-20 23:30:32 -0600

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,14 +1,22 @@
-#list of names from tehe user
-names = ["Bob", "Lily", "Mira", "Logan", "Maggie"]
-swapped = True
-for i in range(0,len(names)):
-    names[i] = names[i].lower()
-while swapped:
-    swapped = False
-    for i in range (len(names)-1):
-        if names[i] > names[i + 1]:
-            swapped = True
-            names[i], names[i + 1] = names[i +1], names[i]
-print(names)
-names.reverse()
-print(names)
+seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print("Available Seating:", seats)
+
+while True:
+    print("Available Seats:", seats)
+    seat_numbers = input("Please type the seat number you would like to purchase: ")
+    if seat_numbers.lower() == "done":
+        break
+    try:
+        seat_number = int(seat_numbers)
+
+        if seat_number < 1 or seat_number > 20:
+            print("Invalid number")
+        elif seat_number in seats:
+            seats.remove(seat_number)
+            print(f"Seat {seat_number} successfully purchased.")
+        else:
+            print(f"Seat {seat_number} is not available. Please select another.")
+    except ValueError:
+        print("Invalid input. Please enter a valid seat number or 'done' to finish.")
+
+print("Updated Available Seating:", seats)
