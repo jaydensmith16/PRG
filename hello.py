c4d005952f589e85c5d263ce1cb53b9c06a45f3c
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-02-21 11:41:41 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,18 +1,11 @@
-# Days of the week
-days = ["Monday", "Tuesday"," Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-# how many steps the person took per day
-step_num = [1200, 3400, 2000, 900, 910, 1000, 800]
-# The numbers are in place of the numbers someone would be putting into this code
-
-for i in range(len(days)):
-    day = days[i]
-    steps = step_num[i]
-    # takes number of steps from the steps list
-    print(f"On {day} how many steps did you take?")
-    print(f"{steps}")
-# total step calculation
-total_steps = sum(step_num)
-print(f"The total steps for the week is {total_steps}.")
-# average steps per day
-average = (total_steps // 7)
-print(f"The average steps per day is {average}.")
+times_slots = ["Morning", "Midday", "Afternoon", "Evening"]
+heart_rate = []
+for time in times_slots:
+    rate = int(input(f"Enter your heart rate for {time}: "))
+    heart_rate.append([time, rate])
+print(heart_rate)
+total = 0
+for rate in heart_rate:
+    total += rate[1]
+average = round(total / len(heart_rate))
+print("average heart rate:", average)0
