5ab4e02f0efa189fb8de639a3b06722437c46e31
jaydensmith16 <157325802+jaydensmith16@users.noreply.github.com>
2024-03-06 11:57:13 -0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-def calculate_interest(calculated_interest):
-    calculated_interest = (input_principal_amount * input_interest_rate * input_investment_time) / 100
-    print(f"The simple interest for a principal amount of ${input_principal_amount:,.2f} \
-at an interest rate of {input_interest_rate}% over a period of {input_investment_time} years is: ${calculated_interest:,.2f}")
-
-input_principal_amount = float(input("Enter the principal amount: "))
-input_interest_rate = float(input("Enter the interest rate: "))
-input_investment_time = float(input("Enter the investment time: "))
-
-calculate_interest(calculated_interest = (input_principal_amount * input_interest_rate * input_investment_time) / 100)
+# factorial function limits
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+# Input and first function put into man function
+def main():
+    n = int(input("Enter a postitive integer: "))
+    result = factorial(n)
+    print("The factorial of", n, "is:", result)
+# calling the main function
+main()
